2d02ac63598e29180d43c59a9e96f22145599be6
Tony Wu <tonytlwu@gmail.com>
2017-05-15 21:22:12 +0100

    Uses data source query provider

--- js/interface.js
+++ js/interface.js
@@ -1,52 +1,76 @@
 var data = Fliplet.Widget.getData() || {
+  dataSourceQuery: undefined,
   showDataLegend: true,
   showDataValues: true,
   showTotalEntries: '',
   autoRefresh: ''
 };
 
-var $dataSource = $('select#select-data-source');
-var $dataColumns = $('select#select-data-column');
-var organizationId = Fliplet.Env.get('organizationId');
-var initialised = false;
+var dsQueryData = {
+  settings: {
+    dataSourceLabel: 'Select a data source',
+    modesDescription: 'How do you want your data to be plotted?',
+    modes: [
+      {
+        label: 'Plot my data as it is',
+        filters: false,
+        columns: [
+          {
+            key: 'categories',
+            label: 'Select the column with the categories',
+            type: 'single'
+          },
+          {
+            key: 'values',
+            label: 'Select the column with the values',
+            type: 'single'
+          }
+        ]
+      },
+      {
+        label: 'Summarise my data',
+        filters: false,
+        columns: [
+          {
+            key: 'column',
+            label: 'Select a column',
+            type: 'single'
+          }
+        ]
+      }
+    ]
+  },
+  result: data.dataSourceQuery
+}
 
-// Fired from Fliplet Studio when the external save button is clicked
-Fliplet.Widget.onSaveRequest(function () {
-  if (!initialised) return Fliplet.Widget.complete();
+var dsQueryProvider = Fliplet.Widget.open('com.fliplet.data-source-query', {
+  selector: ".data-source-query",
+  data: dsQueryData,
+  onEvent: function(event, data){
+    if (event === 'mode-changed') {
+      console.log('mode index', data.value)
+    }
 
+    return true;
+  }
+});
+
+dsQueryProvider.then(function(result){
   Fliplet.Widget.save({
-    dataSourceId: parseInt($dataSource.val(), 10),
-    dataSourceColumn: $dataColumns.val(),
+    dataSourceQuery: result,
     showDataLegend: $('#show_data_legend:checked').val() === "show",
     showDataValues: $('#show_data_values:checked').val() === "show",
     showTotalEntries: $('#show_total_entries:checked').val() === "show",
     autoRefresh: $('#auto_refresh:checked').val() === "refresh"
   }).then(function () {
     Fliplet.Widget.complete();
+    Fliplet.Studio.emit('reload-page-preview');
   });
 });
 
-// GET DATA SOURCES, EXCLUDE MENUS, GET ALL THE ROWS
-Fliplet.DataSources.get({
-  organizationId: organizationId
-}).then(function (dataSources) {
-  data.dataSources = dataSources;
-  var templateSource = $('template[name="dataSourceTemplate"]').html();
-  var template = Handlebars.compile(templateSource);
-  $dataSource.html(template(dataSources));
-
-  // LOADS DATA SOURCE DATA
-  // NEEDS TO BE DONE HERE BECAUSE THE SELECT BOX NEEDS TO BE DYNAMIC UPDATED
-  if (data.dataSourceId) {
-    $dataSource.val(data.dataSourceId);
-    showColumnSelect();
-  }
-  $dataSource.trigger('change');
-  if ( data.dataSourceId && data.dataSourceColumn ) {
-    $dataColumns.val(data.dataSourceColumn).trigger('change');
-  }
-
-  initialised = true;
+// Fired from Fliplet Studio when the external save button is clicked
+Fliplet.Widget.onSaveRequest(function () {
+  dsQueryProvider.forwardSaveRequest();
 });
 
 // LOAD CHART SETTINGS
@@ -55,46 +79,4 @@ if (data) {
   $('#show_data_values').prop('checked', data.showDataValues);
   $('#show_total_entries').prop('checked', data.showTotalEntries);
   $('#auto_refresh').prop('checked', data.autoRefresh);
-}
-
-
-// ATTACH LISTENERS
-$dataSource.on('change', function(){
-  var selectedValue = $(this).val();
-  var selectedText = $(this).find("option:selected").text();
-
-  var templateSource = $('template[name="dataColumnTemplate"]').html();
-  var template = Handlebars.compile(templateSource);
-
-  var dataSource = _.find(data.dataSources, {id: parseInt(selectedValue,10)});
-  if (typeof dataSource !== 'undefined') {
-    $dataColumns.html(template(dataSource.columns));
-    showColumnSelect();
-  }
-
-  $(this).parents('.select-proxy-display').find('.select-value-proxy').html(selectedText);
-  checkDataIsConfigured();
-});
-
-$dataColumns.on('change', function() {
-  var selectedText = $(this).find("option:selected").text();
-  $(this).parents('.select-proxy-display').find('.select-value-proxy').html(selectedText);
-  checkDataIsConfigured();
-});
-
-// FUNCTIONS
-function showColumnSelect() {
-  if ($dataSource.val() !== 'none') {
-    $('.select-data-column').removeClass('hidden');
-  } else {
-    $('.select-data-column').addClass('hidden');
-  }
-}
-
-function checkDataIsConfigured() {
-  if ($dataSource.val() !== '' && $dataColumns.val() !== '') {
-    $('#chart-settings').removeClass('hidden');
-  } else {
-    $('#chart-settings').addClass('hidden');
-  }
 }
