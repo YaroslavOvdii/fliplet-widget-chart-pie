d199a5e428509e6b8e5abfeaf2e173952b8947da
Andrey Riazantsev <71251809+AndrRyaz@users.noreply.github.com>
2020-11-04 16:14:46 +0200

    add access rules to chart pie

--- js/interface.js
+++ js/interface.js
@@ -44,6 +44,15 @@ var dsQueryData = {
       }
     ]
   },
+  accessRules: [
+    {
+      allow: 'all',
+      enabled: true,
+      type: [
+        'select'
+      ]
+    }
+  ],
   result: data.dataSourceQuery
 };
 
